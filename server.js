f461af7ae00da801e429b8be328dc096d13bd66d
Marvellous Giegbefumwen <igmarvvvi@gmail.com>
2024-07-01 22:20:39 +0100

    Updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080; // Change the port to 8080
 
 app.use(bodyParser.json());
 app.set('trust proxy', true);
